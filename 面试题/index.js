e2a10871b7aa097d0a9d61004198af1170a6618b
O-Mr-Wei-O <Sinpo96@163.com>
2019-08-08 09:38:03 +0800

    2019年8月8日09:38:00

--- 面试题/index.js
+++ 面试题/index.js
@@ -28,29 +28,59 @@
  * 3.如果是复杂数据类型， 递归。
  * 4.考虑循环引用的问题
  */
-function deepClone(obj) {
-    /* 正则 */
-    if (obj instanceof RegExp) {
-        return new RegExp(obj);
-    }
-    /* 时间格式 */
-    if (obj instanceof Date) {
-        return new Date(obj);
-    }
-    /* null或者其他不是对象类型的基本数据类型 */
-    if (obj === null || typeof obj !== 'object') {
-        return obj;
-    }
+// function deepClone(obj) {
+//     /* 正则 */
+//     if (obj instanceof RegExp) {
+//         return new RegExp(obj);
+//     }
+//     /* 时间格式 */
+//     if (obj instanceof Date) {
+//         return new Date(obj);
+//     }
+//     /* null或者其他不是对象类型的基本数据类型 */
+//     if (obj === null || typeof obj !== 'object') {
+//         return obj;
+//     }
 
-    /**
-     * 如果obj是数组， 那么 obj.constructor是[Function: Array]
-     * 如果obj是对象， 那么 obj.constructor是[Function: Object]
-     */
-    const t = new obj.constructor(); // new一个新的对象出来（使用原型链上的构造函数）
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            t[key] = deepClone(obj[key]);
-        }
-    }
-    return t;
-}
+//     /**
+//      * 如果obj是数组， 那么 obj.constructor是[Function: Array]
+//      * 如果obj是对象， 那么 obj.constructor是[Function: Object]
+//      */
+//     const t = new obj.constructor(); // new一个新的对象出来（使用原型链上的构造函数）
+//     for (const key in obj) {
+//         if (obj.hasOwnProperty(key)) {
+//             t[key] = deepClone(obj[key]);
+//         }
+//     }
+//     return t;
+// }
+
+/**
+ * 三、call的模拟实现
+ */
+// Function.prototype._call = function(context) {
+//     const [, args] = [...arguments]; // 取出参数
+//     context = context || window; // 上下文
+//     context.fn = this; // 这个this指的是调用_call的函数方法
+//     const res = context.fn(args);
+//     delete context.fn;
+//     return res;
+// }
+
+/**
+ * 四、apply的模拟实现
+ */
+// Function.prototype._apply = function(context) {
+//     const [, args] = [...arguments]; // 取出参数
+//     context = context || window; // 上下文
+//     context.fn = this; // 这个this指的是调用_call的函数方法
+//     let res = undefined;
+//     if (!args) {
+//         /* 说明无入参,如果直接调用...args[0]会报错 */
+//         res = context.fn();
+//     } else {
+//         res = context.fn(...args);
+//     }
+//     delete context.fn;
+//     return res;
+// }
