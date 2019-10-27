ed54c5a70d860010afcf4637c5df3b7802c2a5a3
Sinpo96 <Sinpo96@163.com>
2019-10-28 01:02:59 +0800

    插入排序

--- 排序算法/插入排序.js
+++ 排序算法/插入排序.js
@@ -2,24 +2,23 @@
  * @Description: 插入排序
  * @Author: Sinpo
  * @Date: 2019-09-08 21:19:56
- * @LastEditTime: 2019-09-08 21:50:23
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2019-10-28 00:58:48
+ * @LastEditors: Sinpo
  */
-// 插入排序：从头开始，如果前面值比当前大，那么当前值与前一个值互换，依次往前换，知道前面一个小于它
+// 插入排序：打牌插牌，排序完的牌应该是从小到大
 const Insert = (arr) => {
+    console.time('插入排序耗时：');
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < arr[i - 1]) {
-            let inner = i;
-            let temp = arr[inner];
-            while (inner > 0 && arr[inner - 1] > temp) {
-                // 说明比前一个小，前面的值赋给它
-                arr[inner] = arr[inner - 1];
-                inner--;
-            }
-            arr[inner] = temp;
+        const key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j --;
         }
+        arr[j + 1] = key;
     }
     return arr;
 }
+
 let arr = [1, 3, 9, 28, 11, 69, 54, 99, 31]
 console.log(Insert(arr));
