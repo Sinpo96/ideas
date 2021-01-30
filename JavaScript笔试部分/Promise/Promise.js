dbd325e457089c1e23000f1121a8e45cf1cee153
sinpo0 <sinpo96@163.com>
2021-01-31 00:28:40 +0800

    feat：初版Promise的实现，还需要改进

---
+++ JavaScript笔试部分/Promise/Promise.js
@@ -0,0 +1,116 @@
+/**
+ * Promise 的实现核心之处便是回调函数的处理，如何进行存储、当回调函数是Promise对象或者thenable对象时该如何处理...
+ * 并不是一开始想象的递归
+ */
+
+/* --- 预定义三种 promise 状态 --- */
+/**
+ * 处于等待态时，promise 需满足以下条件：
+ *      可以迁移至执行态或拒绝态
+ */
+const PENDING = 'pending';
+
+/**
+ * 处于执行态时，promise 需满足以下条件：
+ *      不能迁移至其他任何状态
+ *      必须拥有一个不可变的终值
+ */
+const FULFILLED = 'fulfilled';
+
+/**
+ * 处于拒绝态时，promise 需满足以下条件：
+ *      不能迁移至其他任何状态
+ *      必须拥有一个不可变的据因
+ */
+const REJECTED = 'rejected';
+
+const isFunction = arg => typeof arg === 'function';
+
+/**
+ * @desc Promise状态变更的回调处理函数
+ */
+const handleCallback = (state, result, callback) => {
+    const { onFulfilled, onRejected, resolve, reject } = callback;
+    if (state === FULFILLED) {
+        isFunction(onFulfilled) ? resolve(onFulfilled(result)) : resolve(result);
+    } else if (state === REJECTED) {
+        // 如果 onReject 是 function，那么执行完的值 resolve 掉，如果不是，那么还得传递给下一个 then 进行处理
+        isFunction(onRejected) ? resolve(onRejected(result)) : reject(result);
+    }
+}
+
+const handlePromiseCallbacks = (promise) => {
+    const { state, result, callbacks } = promise;
+    while (callbacks.length) {
+        const callback = callbacks.shift();
+        handleCallback(state, result, callback);
+    }
+};
+
+/**
+ * @desc promise的实现源码
+ * @constructor
+ */
+function PromiseSrc (fn) {
+    // 默认为等待态
+    this.state = PENDING;
+    this.result = null;
+    this.callbacks = [];
+
+    /**
+     * @desc resolve, value 指任何 JavaScript 的合法值（包括 undefined , thenable 和 promise）；
+     * @param value
+     */
+    const resolve = (value) => {
+        this.result = value;
+        this.state = FULFILLED;
+        handlePromiseCallbacks(this);
+    };
+
+    /**
+     * @desc reject, reason 表示一个 promise 的拒绝原因。
+     * @param reason
+     */
+    const reject = (reason) => {
+        this.result = reason;
+        this.state = REJECTED;
+        handlePromiseCallbacks(this);
+    };
+
+    fn(resolve, reject);
+}
+
+/**
+ * @desc then
+ * @param onFulfilled（可选）   如果 onFulfilled 不是函数，其必须被忽略；如果是函数，那么在 Promise 执行执行结束后必须被调用，第一个参数为 Promise 的终值
+ * @param onRejected（可选）    如果 onRejected 不是函数，其必须被忽略；如果是函数，那么在 Promise 执行执行结束后必须被调用，第一个参数为 Promise 的拒因
+ */
+PromiseSrc.prototype.then = function (onFulfilled, onRejected) {
+    // 两个形参，对应着 fn(resolve, reject)
+    return new PromiseSrc((resolve, reject) => {
+        const callback = { onFulfilled, onRejected, resolve, reject };
+        if (this.state === PENDING) {
+            // 等待态，回调函数放入callbacks中
+            this.callbacks.push(callback);
+        } else {
+            // 说明状态已经变更了
+            setTimeout(() => {
+                handleCallback(this.state, this.result, callback);
+            }, 0);
+        }
+    });
+}
+
+new PromiseSrc((resolve, reject) => {
+    reject('test');
+}).then((val) => {
+    console.log(val);
+    return val;
+}).then((val) => {
+    console.log(val)
+}, (val) => {
+    console.log('reject')
+    console.log(val)
+});
+
+// module.exports = PromiseSrc;
