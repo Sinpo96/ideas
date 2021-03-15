1936cf5cfcce96651bbfdd7fab47dab414462dcb
Sinpo96 <Sinpo96@163.com>
2021-03-15 23:29:17 +0800

    Update 189、旋转数组.js

--- 算法/189、旋转数组.js
+++ 算法/189、旋转数组.js
@@ -1,21 +1,31 @@
-const gcd = (x, y) => y ? gcd(y, x % y) : x;
-
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 var rotate = function(nums, k) {
-    // debugger
-    const n = nums.length;
-    k = k % n;
-    let count = gcd(k, n);
-    for (let start = 0; start < count; ++start) {
-        let current = start;
-        let prev = nums[start];
-        do {
-            const next = (current + k) % n;
-            const temp = nums[next];
-            nums[next] = prev;
-            prev = temp;
-            current = next;
-        } while (start !== current);
+    // 约瑟夫环
+    // hold保存了中间值(默认是第一个)
+    let hold = nums[0];
+    let index = 0;
+    const len = nums.length;
+    let visited = Array(len).fill(false);
+    for(let i = 0; i < nums.length; i++) {
+        // 得出当前应该被替换的位置
+        index = (index + k) % len;
+        if (visited[index]) {
+            // index 向后一位
+            index = (index + 1) % len;
+            hold = nums[index];
+            // 这次不算，不至于加到越界
+            i--;
+        } else {
+            visited[index] = true;
+            let temp = hold;
+            hold = nums[index];
+            nums[index] = temp;
+        }
     }
 };
 
-rotate([1,2,3], 1)
+rotate([-1,-100,3,99], 2)
