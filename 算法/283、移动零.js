e2898621c66d3d84b7324ccc4d69e5149a9e5fe1
sinpo0 <Sinpo96@163.com>
2021-03-16 21:59:57 +0800

    Create 283、移动零.js

---
+++ 算法/283、移动零.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+	let i = 0, j = 1;
+	while (j < nums.length) {
+		if (nums[i] !== 0) {
+			i++;
+		} else if (nums[i] === 0 && nums[j] !== 0) {
+			[nums[i], nums[j]] = [nums[j], nums[i]];
+			i++;
+		}
+		j++;
+	}
+	return nums;
+};
+
+moveZeroes([1, 2, 0]);
