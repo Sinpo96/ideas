2b1e853f86742a78de20c875b8b41c9881f5cfae
sinpo0 <Sinpo96@163.com>
2021-04-05 22:22:59 +0800

    fix：缩进修改

--- JavaScript笔试部分/深拷贝/BFS.js
+++ JavaScript笔试部分/深拷贝/BFS.js
@@ -1,12 +1,12 @@
 const obj = {
-	a1: 1,
-	a2: {
-		b1: 1,
-		b2: {
-			c1: 1
-		},
-		b3: [124]
-	},
+    a1: 1,
+    a2: {
+        b1: 1,
+        b2: {
+            c1: 1
+        },
+        b3: [124]
+    },
 };
 obj.a3 = obj;
 
@@ -30,13 +30,13 @@ const isArray = val => Object.prototype.toString.call(val) === '[object Array]';
  * @returns {{}}
  */
 const getEmpty = (val) => {
-	if (isObject(val)) {
-		return {};
-	}
-	if (isArray(val)) {
-		return [];
-	}
-	return val;
+    if (isObject(val)) {
+        return {};
+    }
+    if (isArray(val)) {
+        return [];
+    }
+    return val;
 }
 
 /**
@@ -44,42 +44,42 @@ const getEmpty = (val) => {
  * @param obj
  */
 const deepCloneBFS = (obj) => {
-	// 使用map破解递归爆栈
-	const map = new Map();
-	// 队列保存待遍历的数据
-	let queue = [];
-	// 返回值的类型根据obj的类型来定
-	let res = getEmpty(obj);
-	// 如果相等，说明是同一个数据，不用遍历（例如传入1，getEmpty返回的也是1，这就不需要遍历了）
-	if (res !== obj) {
-		queue.push([obj, res]);
-		map.set(obj, res);
-	}
-	while (queue.length) {
-		// 取出第一个节点
-		const [node, target] = queue.shift();
-		for (let key in node) {
-			if (!Object.prototype.hasOwnProperty.call(node, key)) {
-				continue;
-			}
-			if (map.get(node[key])) {
-				// 存在说明是循环引用了，直接返回对象就行了
-				target[key] = map.get(node[key]);
-				continue;
-			}
-			// 初始化target[key]
-			target[key] = getEmpty(node[key]);
-			// 引用类型
-			if (isObject(node[key]) || isArray(node[key])) {
-				queue.push([node[key], target[key]]);
-				map.set(node[key], target[key]);
-				continue;
-			}
-			// 走到这里的都是基础类型了，不会有引用，直接赋值
-			target[key] = node[key];
-		}
-	}
-	return res;
+    // 使用map破解递归爆栈
+    const map = new Map();
+    // 队列保存待遍历的数据
+    let queue = [];
+    // 返回值的类型根据obj的类型来定
+    let res = getEmpty(obj);
+    // 如果相等，说明是同一个数据，不用遍历（例如传入1，getEmpty返回的也是1，这就不需要遍历了）
+    if (res !== obj) {
+        queue.push([obj, res]);
+        map.set(obj, res);
+    }
+    while (queue.length) {
+        // 取出第一个节点
+        const [node, target] = queue.shift();
+        for (let key in node) {
+            if (!Object.prototype.hasOwnProperty.call(node, key)) {
+                continue;
+            }
+            if (map.get(node[key])) {
+                // 存在说明是循环引用了，直接返回对象就行了
+                target[key] = map.get(node[key]);
+                continue;
+            }
+            // 初始化target[key]
+            target[key] = getEmpty(node[key]);
+            // 引用类型
+            if (isObject(node[key]) || isArray(node[key])) {
+                queue.push([node[key], target[key]]);
+                map.set(node[key], target[key]);
+                continue;
+            }
+            // 走到这里的都是基础类型了，不会有引用，直接赋值
+            target[key] = node[key];
+        }
+    }
+    return res;
 }
 
 deepCloneBFS(obj);
--- JavaScript笔试部分/深拷贝/DFS.js
+++ JavaScript笔试部分/深拷贝/DFS.js
@@ -1,12 +1,12 @@
 const obj = {
-	a1: 1,
-	a2: {
-		b1: 1,
-		b2: {
-			c1: 1
-		},
-		b3: [124]
-	},
+    a1: 1,
+    a2: {
+        b1: 1,
+        b2: {
+            c1: 1
+        },
+        b3: [124]
+    },
 };
 obj.a3 = obj;
 
@@ -30,13 +30,13 @@ const isArray = val => Object.prototype.toString.call(val) === '[object Array]';
  * @returns {{}}
  */
 const getEmpty = (val) => {
-	if (isObject(val)) {
-		return {};
-	}
-	if (isArray(val)) {
-		return [];
-	}
-	return val;
+    if (isObject(val)) {
+        return {};
+    }
+    if (isArray(val)) {
+        return [];
+    }
+    return val;
 }
 
 /**
@@ -44,44 +44,44 @@ const getEmpty = (val) => {
  * @param obj
  */
 const deepCloneDFS = (obj) => {
-	const map = new Map();
-	let queue = [];
-	let res = getEmpty(obj);
+    const map = new Map();
+    let queue = [];
+    let res = getEmpty(obj);
 
-	if (res !== obj) {
-		// 不等于说明obj是引用类型，需要遍历
-		queue.push([obj, res]);
-		map.set(obj, res);
-	}
+    if (res !== obj) {
+        // 不等于说明obj是引用类型，需要遍历
+        queue.push([obj, res]);
+        map.set(obj, res);
+    }
 
-	while (queue.length) {
-		// 取出第一个节点
-		const [node, target] = queue.shift();
-		const temporaryQueue = [];
-		for (let key in node) {
-			if (!Object.prototype.hasOwnProperty.call(node, key)) continue;
-			// 破解循环引用
-			if (map.get(node[key])) {
-				target[key] = map.get(node[key]);
-				continue;
-			}
-			target[key] = getEmpty(node[key]);
-			// 如果是引用类型
-			if (isArray(node[key]) || isObject(node[key])) {
-				// 与广度优先遍历的区别就是这一行----------------------------------------------------
-				// 广度是每一层遍历完再去遍历下一层；深度是只要这层有，就一直遍历
-				temporaryQueue.push([node[key], target[key]]);
-				map.set(node[key], target[key]);
-				continue;
-			}
-			// 走到这里都是基础类型了，直接赋值吧
-			target[key] = node[key];
-		}
-		// 使用临时队列按顺序储存此次的待遍历数据，形成  先序优先遍历：根->左->由
-		queue = [...temporaryQueue, ...queue];
-	}
+    while (queue.length) {
+        // 取出第一个节点
+        const [node, target] = queue.shift();
+        const temporaryQueue = [];
+        for (let key in node) {
+            if (!Object.prototype.hasOwnProperty.call(node, key)) continue;
+            // 破解循环引用
+            if (map.get(node[key])) {
+                target[key] = map.get(node[key]);
+                continue;
+            }
+            target[key] = getEmpty(node[key]);
+            // 如果是引用类型
+            if (isArray(node[key]) || isObject(node[key])) {
+                // 与广度优先遍历的区别就是这一行----------------------------------------------------
+                // 广度是每一层遍历完再去遍历下一层；深度是只要这层有，就一直遍历
+                temporaryQueue.push([node[key], target[key]]);
+                map.set(node[key], target[key]);
+                continue;
+            }
+            // 走到这里都是基础类型了，直接赋值吧
+            target[key] = node[key];
+        }
+        // 使用临时队列按顺序储存此次的待遍历数据，形成  先序优先遍历：根->左->由
+        queue = [...temporaryQueue, ...queue];
+    }
 
-	return res;
+    return res;
 }
 
 deepCloneDFS(obj);
