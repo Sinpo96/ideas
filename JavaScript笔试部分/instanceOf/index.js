6ad6737f1a9cc7b548ba7621865d6930c2637e2b
sinpo0 <Sinpo96@163.com>
2021-04-05 22:32:02 +0800

    fix：直接判断L存在就行了

--- JavaScript笔试部分/instanceOf/index.js
+++ JavaScript笔试部分/instanceOf/index.js
@@ -1,24 +1,22 @@
 const instanceOf = (Left, Right) => {
     let L = Left.__proto__; // 取Left的隐式原型
     let R = Right.prototype; // 取Right的显式原型
-    while (true) {
-        if (L === null) {
-            return false;
-        }
+    while (L) {
         if (L === R) {
             return true;
         } else {
             L = L.__proto__;
-        }   
+        }
     }
+    return false;
 }
 
-function a() {
-    
+function a () {
+
 }
 
-function b() {
-    
+function b () {
+
 }
 
 const c = new a();
